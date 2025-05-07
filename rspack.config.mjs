d9c86e55fbe9e47976e4cd5d8f9d4bb48fda2fef
dfoxpowell <dfoxpowell@hubspot.com>
2025-05-07 15:28:52 -0700

    add alias example

--- rspack.config.mjs
+++ rspack.config.mjs
@@ -9,6 +9,35 @@ if (!isRunningRspack && !isRunningWebpack) {
   throw new Error("Unknown bundler");
 }
 
+const alias = {
+  'alias': 'alias-resolve',
+};
+
+class ResolverPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap(
+      ResolverPlugin.name,
+      (compilation, { normalModuleFactory }) => {
+        normalModuleFactory.hooks.beforeResolve.tap(
+          ResolverPlugin.name,
+          (resolveData) => {
+            if (resolveData.request === 'alias') {
+              // Ideally if we return here the alias would be processed and we would received a subsequent request
+              // for 'alias-resolve' and we could resolve the full path to that.
+              return;
+            }
+
+            if (resolveData.request === 'direct-resolve' || resolveData.request === 'alias-resolve') {
+              console.log('resolveData', resolveData.request)
+              resolveData.request = path.resolve(__dirname, 'src', `${resolveData.request}.js`)
+            }
+          }
+        );
+      }
+    );
+  }
+}
+
 /**
  * @type {import('webpack').Configuration | import('@rspack/cli').Configuration}
  */
@@ -18,7 +47,8 @@ const config = {
   entry: {
     main: "./src/index",
   },
-  plugins: [new HtmlWebpackPlugin()],
+  resolve: {alias},
+  plugins: [new HtmlWebpackPlugin(), new ResolverPlugin()],
   output: {
     clean: true,
     path: isRunningWebpack
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import directResolve from 'direct-resolve';
+import aliasResolve from 'alias';
+
+console.log('directResolve', directResolve);
+console.log('aliasResolve', aliasResolve);
+
 import('./render').then(exports => {
     exports.render()
 })
